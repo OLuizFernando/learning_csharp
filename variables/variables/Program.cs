8b25f207f96ba6a3b1df7a33940ba853187ff03c
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-08-11 19:09:05 -0300

    diferences between 'var', 'dynamic' and 'const' declarations

--- variables/variables/Program.cs
+++ variables/variables/Program.cs
@@ -10,15 +10,21 @@ namespace variables
     {
         static void Main(string[] args)
         {
+            // strong typing
             int age = 18;
             string name = "Luiz Fernando";
             float height = 1.73f; // float numbers must have "f" at the end
             bool isEmployed = false;
+            var exampleVar = "example"; // the type is defined when assigning a value
+            const float exampleConst = 3.14159f; // consts must have a predefined type and the value can't be modified
+
+            // weak typing
+            dynamic exampleDynamic = 123;
+            exampleDynamic = "one two three"; // the type can be modified
 
             Console.WriteLine("Hi! My name's {0}, I'm {1} years old and I'm {2}m tall.", name, age, height); // placeholders must have an index
 
             Console.Write("At the moment, ");
-
             if (isEmployed)
             {
                 Console.WriteLine("I'm employed. :)");
