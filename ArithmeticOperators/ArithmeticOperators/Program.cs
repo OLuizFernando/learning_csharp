febbbce64059eeb45f1aae1699ea9514d73b05ec
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-08-11 20:27:09 -0300

    differences between int and float divisions

---
+++ ArithmeticOperators/ArithmeticOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int exampleAddition = 5 + 2;
+            int exampleSubtraction = 5 - 2;
+            int exampleMultiplication = 5 * 2;
+
+            // differences in divisions
+            int exampleIntDivision = 5 / 2; // int 5 / 2 = 2
+            float exampleFloatDivision = 5 / 2; // float 5 / 2 = 2
+            exampleFloatDivision = 5f / 2; // float 5f / 2 = 2.5
+
+            Console.WriteLine(exampleFloatDivision);
+            Console.ReadKey();
+        }
+    }
+}
